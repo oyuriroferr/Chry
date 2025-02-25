f5bb060b50f64a9093038e3275a281795dc69014
Yuri Ferreira <80469922+oyuriroferr@users.noreply.github.com>
2025-02-25 17:43:36 -0300

    Update remove.cmd

--- remove.cmd
+++ remove.cmd
@@ -3,25 +3,37 @@
 :: Lê o caminho do Python do arquivo python.txt
 set /p DIR=<"%USERPROFILE%\Portable\python.txt"
 
+:: Verifica se a variável DIR está vazia
+if "%DIR%"=="" (
+    cls
+    echo Nao foi possivel achar o caminho do Python
+    goto end_bad
+)
+
 :: Verifica se a pasta venv existe
 if exist "%USERPROFILE%\Portable\venv\" (
     :: Deleta a pasta Portable
+    cls
     RD /S /Q "%USERPROFILE%\Portable"
-    echo Venv Version
-    :: Verifica se houve erro ao excluir o diretório
-    if %errorlevel% NEQ 0 (
-        echo Erro ao Deletar Venv Version
-    )
-) else (
-    :: Desinstala os pacotes globalmente
-    "%DIR%" -m pip uninstall -y pywin32 pynput pygame pyautogui psutil ffpyplayer pywin32 
-    :: Deleta a pasta Portable
-    RD /S /Q "%USERPROFILE%\Portable"
-    echo Global Version
-    :: Verifica se houve erro ao excluir o diretório
-    if %errorlevel% NEQ 0 (
-        echo Erro ao Deletar Global Version
-    )
+    echo Venv Version Deleted
+    goto end_good
 )
 
+:: Se não encontrou a pasta venv, verifica a pasta Portable
+if exist "%USERPROFILE%\Portable\" (
+    :: Desinstala os pacotes globalmente
+    cls
+    "%DIR%" -m pip uninstall -y pywin32 pynput pygame pyautogui psutil ffpyplayer pywin32
+    :: Deleta a pasta Portable
+    RD /S /Q "%USERPROFILE%\Portable"
+    echo Global Version Deleted
+    goto end_good
+)
+
+:end_good
+echo Goodbye!!!
+pause
+
+:end_bad
+echo Fudeu nego!
 pause
